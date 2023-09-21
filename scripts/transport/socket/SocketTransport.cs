9aa34428bb180d9ef4ea9b0069dd706c5e16668e
Marek Mackulin <mackulinm@gmail.com>
2023-09-21 12:54:27 +0200

    Update SocketTransport.cs

--- scripts/transport/socket/SocketTransport.cs
+++ scripts/transport/socket/SocketTransport.cs
@@ -1,67 +1,71 @@
-﻿using System;
+using System;
+using System.Net;
 using System.Net.Sockets;
-using Nez;
-
-namespace nez.net.transport.socket;
+using ZeroFormatter;
 
 public class SocketTransport : ITransport
 {
-    private TcpClient _client;
-    private NetworkStream _stream;
-    private bool _isConnected;
-    
-    public void Connect(string address)
+    private Socket _serverSocket;
+    private Socket _clientSocket;
+    private IPEndPoint _ipEndPoint;
+
+    // Initialize the server socket and start listening
+    public void InitializeServer(int port)
     {
-        try
-        {
-            _client = new TcpClient(address, 7777);
-            _stream = _client.GetStream();
-            _isConnected = true;
-        }
-        catch (Exception e)
-        {
-            Debug.Error($"Could not connect to server {e.Message}");
-        }
-    }
-    
-    public void Disconnect()
-    {
-        _stream.Close();
-        _client.Close();
-        _isConnected = false;
-    }
-    
-    public bool IsConnected()
-    {
-        return _isConnected;
-    }
-    
-    public void Send(byte[] data, int channelId)
-    {
-        if(_isConnected)
-        {
-            _stream.Write(data, 0, data.Length);
-        }
+        _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        _ipEndPoint = new IPEndPoint(IPAddress.Any, port);
+        _serverSocket.Bind(_ipEndPoint);
+        _serverSocket.Listen(10);
     }
 
-    public bool TryReceive(out byte[] data, out int channelId)
+    // Initialize the client socket and connect to the server
+    public void InitializeClient(string ipAddress, int port)
     {
-        data = null;
-        channelId = 0;
-        
-        if(!_isConnected)
-        {
-            return false;
-        }
-        
-        if(_stream.DataAvailable)
-        {
-            data = new byte[1024]; // max size of packet?
-            int bytesRead = _stream.Read(data, 0, data.Length);
-            Array.Resize(ref data, bytesRead);
-            return true;
-        }
+        _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        _clientSocket.Connect(ipAddress, port);
+    }
 
-        return false;
+    public void ServerSend(NetMessage message)
+    {
+        byte[] serializedMessage = ZeroFormatterSerializer.Serialize(message);
+        _serverSocket.Send(serializedMessage);
+    }
+
+    public NetMessage ServerReceive()
+    {
+        byte[] buffer = new byte[1024];
+        int receivedLength = _serverSocket.Receive(buffer);
+
+        byte[] actualReceived = new byte[receivedLength];
+        Array.Copy(buffer, actualReceived, receivedLength);
+
+        return ZeroFormatterSerializer.Deserialize<NetMessage>(actualReceived);
+    }
+
+    public void ClientSend(NetMessage message)
+    {
+        byte[] serializedMessage = ZeroFormatterSerializer.Serialize(message);
+        _clientSocket.Send(serializedMessage);
+    }
+
+    public NetMessage ClientReceive()
+    {
+        byte[] buffer = new byte[1024];
+        int receivedLength = _clientSocket.Receive(buffer);
+
+        byte[] actualReceived = new byte[receivedLength];
+        Array.Copy(buffer, actualReceived, receivedLength);
+
+        return ZeroFormatterSerializer.Deserialize<NetMessage>(actualReceived);
+    }
+
+    public void CloseServer()
+    {
+        _serverSocket.Close();
+    }
+
+    public void CloseClient()
+    {
+        _clientSocket.Close();
     }
 }
