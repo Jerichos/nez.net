5a2fbda79237065ffe681e7a50292dd028f680c1
Marek Mackulin <mackulinm@gmail.com>
2023-09-21 11:58:09 +0200

    Update NetworkManager.cs

--- scripts/NetworkManager.cs
+++ scripts/NetworkManager.cs
@@ -4,7 +4,21 @@ namespace nez.net;
 
 public class NetworkManager
 {
+    private static NetworkManager _instance;
+    public static NetworkManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+                _instance = new NetworkManager();
+
+            return _instance;
+        }
+    }
+    
     private ITransport _transport;
+
+    public NetworkManager(){}
     
     public NetworkManager(ITransport transport)
     {
